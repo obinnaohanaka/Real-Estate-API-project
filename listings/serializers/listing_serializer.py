0118e99affa0ecff7a044a66d1daee9af1a6aed0
Obinna Ohanaka <obinna.c.ohanaka@gmail.com>
2026-08-23 18:10:24 +0100

    updated listing_serializer.py

--- listings/serializers/listing_serializer.py
+++ listings/serializers/listing_serializer.py
@@ -4,11 +4,11 @@ from listings.models import Listing
 
 
 class ListingSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Listing
         fields = "__all__"
         read_only_fields = (
+            "id",
             "agent",
             "created_at",
             "updated_at",
